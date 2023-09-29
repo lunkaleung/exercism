2e61d2b1ea781bb2a7271fcae54c636cd6bd558a
davidlun <davidlun@solutions.local>
2023-09-29 14:38:32 +0800

    update

--- Practice/Go/lasagna.go
+++ Practice/Go/lasagna.go
@@ -1,21 +1,25 @@
-package main
+package lasagna
 
 import "fmt"
 
+const OvenTime int = 40
+
 func main() {
 	fmt.Println(RemainingOvenTime(30))
+	fmt.Println(PreparationTime(2))
+	fmt.Println(ElapsedTime(3, 20))
 }
 
 func RemainingOvenTime(actual int) int {
-	const OvenTime int = 40
 
 	return OvenTime - actual
 }
 
 func PreparationTime(numberOfLayers int) int {
-	return numberOfLayers + 2
+	return numberOfLayers * 2
 }
 
+// ElapsedTime calculates the time elapsed cooking the lasagna. This time includes the preparation time and the time the lasagna is baking in the oven.
 func ElapsedTime(numberOfLayers, actualMinutesInOven int) int {
 	return (numberOfLayers*2 + actualMinutesInOven)
 }
