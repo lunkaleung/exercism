bd221f4230e9da0ebd625ea30de0b77b6d7c32a9
davidlun <davidlun@solutions.local>
2023-09-29 14:03:34 +0800

    update

--- Practice/Go/Basics.go
+++ Practice/Go/Basics.go
@@ -4,18 +4,16 @@ import "fmt"
 
 func main() {
 
-    fmt.Println(RemainingOvenTime(30))
-        
+	fmt.Println(RemainingOvenTime(30))
+
 }
 
-
 func RemainingOvenTime(actual int) int {
-    const OvenTime int = 40
+	const OvenTime int = 40
 
-    return OvenTime - actual
+	return OvenTime - actual
 }
 
 func PreparationTime(numberOfLayers int) int {
-    return numberOfLayers + 2 
+	return numberOfLayers + 2
 }
-
